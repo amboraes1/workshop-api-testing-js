df1606b60fcb8077c92a36b3b63e245d4ab270ad
Tomás Alvarez Gomez <53188146+amboraes1@users.noreply.github.com>
2024-09-09 11:36:53 -0500

    Adding eslint (#6)
    * Adding eslint
    
    * a

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -1,71 +1,71 @@
-const axios = require("axios");
-const { expect } = require("chai");
+const axios = require('axios');
+const { expect } = require('chai');
 
-describe("First Api Test", () => {
+describe('First Api Test', () => {
   let request;
 
-  it("Consume GET Service", async () => {
-    const response = await axios.get("https://httpbin.org/ip");
+  it('Consume GET Service', async () => {
+    const response = await axios.get('https://httpbin.org/ip');
 
     expect(response.status).to.eq(200);
-    expect(response.data).to.have.property("origin");
+    expect(response.data).to.have.property('origin');
   });
 
-  it("Consume GET Service with query parameters ", async () => {
+  it('Consume GET Service with query parameters ', async () => {
     const query = {
-      name: "John,",
-      age: "31",
-      city: "New York",
+      name: 'John,',
+      age: '31',
+      city: 'New York'
     };
-    const response = await axios.get("https://httpbin.org/get", { query });
+    const response = await axios.get('https://httpbin.org/get', { query });
     expect(response.status).to.eq(200);
     expect(response.config.query).to.eql(query);
   });
 
-  it("Consume HEAD request", async () => {
+  it('Consume HEAD request', async () => {
     request = {
-      url: `/`,
-      method: "HEAD",
-      baseURL: "https://httpbin.org/",
+      url: '/',
+      method: 'HEAD',
+      baseURL: 'https://httpbin.org/',
       headers: {
-        "Content-Type": "application/json",
-      },
+        'Content-Type': 'application/json'
+      }
     };
     const response = await axios(request);
     expect(response.status).eq(200);
   });
-  it("Consume PATCH request", async () => {
+  it('Consume PATCH request', async () => {
     request = {
-      url: `/patch`,
-      method: "PATCH",
-      baseURL: "https://httpbin.org/",
+      url: '/patch',
+      method: 'PATCH',
+      baseURL: 'https://httpbin.org/',
       headers: {
-        "Content-Type": "application/json",
-      },
+        'Content-Type': 'application/json'
+      }
     };
     const response = await axios(request);
     expect(response.status).eq(200);
   });
-  it("Consume PUT request", async () => {
+  it('Consume PUT request', async () => {
     request = {
-      url: `/put`,
-      method: "PUT",
-      baseURL: "https://httpbin.org/",
+      url: '/put',
+      method: 'PUT',
+      baseURL: 'https://httpbin.org/',
       headers: {
-        "Content-Type": "application/json",
-      },
+        'Content-Type': 'application/json'
+      }
     };
     const response = await axios(request);
     expect(response.status).eq(200);
   });
-  it("Consume DELETE request", async () => {
+  it('Consume DELETE request', async () => {
     request = {
-      url: `/delete`,
-      method: "DELETE",
-      baseURL: "https://httpbin.org/",
+      url: '/delete',
+      method: 'DELETE',
+      baseURL: 'https://httpbin.org/',
       headers: {
-        "Content-Type": "application/json",
-      },
+        'Content-Type': 'application/json'
+      }
     };
     const response = await axios(request);
     expect(response.status).eq(200);
